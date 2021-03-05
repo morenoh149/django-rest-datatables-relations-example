660ba09c10797c615e008bb348d66519f3b46597
Brandon Aldrich <brandon@aldr.io>
2021-03-05 17:11:10 -0500

    change ExpertGlobalFilter to a DatatablesFilterSet

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django_filters import filters
 from rest_framework import viewsets
 from rest_framework_datatables.django_filters.backends import DatatablesFilterBackend
+from rest_framework_datatables.django_filters.filterset import DatatablesFilterSet
 from rest_framework_datatables.django_filters.filters import GlobalFilter
 
 from pages.models import Expert, Meeting
@@ -16,9 +17,9 @@ class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
-class ExpertGlobalFilter(GlobalFilter):
+class ExpertGlobalFilterSet(DatatablesFilterSet):
     name = GlobalCharFilter(lookup_expr='icontains')
-    objectives = GlobalCharFilter(field_name='meetings__objective', lookup_expr='icontains')
+    objectives = GlobalCharFilter(field_name='meeting__objective', lookup_expr='icontains')
 
     class Meta:
         model = Expert
@@ -28,7 +29,7 @@ class ExpertViewSet(viewsets.ModelViewSet):
     queryset = Expert.objects.all().order_by("id")
     serializer_class = ExpertSerializer
     filter_backends = (DatatablesFilterBackend,)
-    filterset_class = ExpertGlobalFilter
+    filterset_class = ExpertGlobalFilterSet
 
 
 class MeetingViewSet(viewsets.ModelViewSet):
